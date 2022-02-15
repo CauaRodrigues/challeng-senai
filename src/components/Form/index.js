a34ddaf78c9d2141d68b3850cf33b82947c40e57
CauaRodrigues <caua.rodriguesassis205@gmail.com>
2022-02-14 23:59:12 -0300

    campo de senha

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,6 +1,15 @@
 import React from "react";
 import TextField from "@mui/material/TextField";
 import { styled } from "@mui/material/styles";
+
+// import IconButton from "@mui/material/IconButton";
+// import OutlinedInput from "@mui/material/OutlinedInput";
+// import InputLabel from "@mui/material/InputLabel";
+// import InputAdornment from "@mui/material/InputAdornment";
+// import FormControl from "@mui/material/FormControl";
+// import Visibility from "@mui/icons-material/Visibility";
+// import VisibilityOff from "@mui/icons-material/VisibilityOff";
+
 import "./styles/style.css";
 
 const Fields = styled(TextField)({
@@ -32,6 +41,29 @@ const Fields = styled(TextField)({
 });
 
 const Form = () => {
+	// const [values, setValues] = useState({
+	// 	amount: "",
+	// 	password: "",
+	// 	weight: "",
+	// 	weightRange: "",
+	// 	showPassword: false,
+	// });
+
+	// const handleChange = (prop) => (event) => {
+	// 	setValues({ ...values, [prop]: event.target.value });
+	// };
+
+	// const handleClickShowPassword = () => {
+	// 	setValues({
+	// 		...values,
+	// 		showPassword: !values.showPassword,
+	// 	});
+	// };
+
+	// const handleMouseDownPassword = (event) => {
+	// 	event.preventDefault();
+	// };
+
 	return (
 		<section className="form">
 			<div className="form-hero">
@@ -62,6 +94,31 @@ const Form = () => {
 							variant="outlined"
 							type="password"
 						/>
+
+						{/* <FormControl variant="outlined">
+							<InputLabel htmlFor="outlined-adornment-password">
+								Password
+							</InputLabel>
+							<OutlinedInput
+								id="outlined-adornment-password"
+								type={values.showPassword ? "text" : "password"}
+								value={values.password}
+								onChange={handleChange("password")}
+								endAdornment={
+									<InputAdornment position="end">
+										<IconButton
+											aria-label="toggle password visibility"
+											onClick={handleClickShowPassword}
+											onMouseDown={handleMouseDownPassword}
+											edge="end"
+										>
+											{values.showPassword ? <VisibilityOff /> : <Visibility />}
+										</IconButton>
+									</InputAdornment>
+								}
+								label="Password"
+							/>
+						</FormControl> */}
 					</div>
 				</div>
 
