59298dc1408fae20171c9ec4ae4d8714947e01d0
CauaRodrigues <caua.rodriguesassis205@gmail.com>
2022-02-18 17:16:11 -0300

    links

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import SearchIcon from "@mui/icons-material/Search";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import "./styles/style.css";
@@ -10,93 +10,105 @@ import { Avatar } from "@mui/material";
 import SettingsIcon from "@mui/icons-material/Settings";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import LogoutIcon from "@mui/icons-material/Logout";
+import HomeIcon from "@mui/icons-material/Home";
 
 const Header = () => {
-	const { username } = useParams();
+  const { username } = useParams();
 
-	const [anchorEl, setAnchorEl] = React.useState(null);
+  const [anchorEl, setAnchorEl] = React.useState(null);
 
-	const handleClick = (event) => {
-		setAnchorEl(anchorEl ? null : event.currentTarget);
-	};
+  const handleClick = (event) => {
+    setAnchorEl(anchorEl ? null : event.currentTarget);
+  };
 
-	const open = Boolean(anchorEl);
-	const id = open ? "simple-popper" : undefined;
+  const open = Boolean(anchorEl);
+  const id = open ? "simple-popper" : undefined;
 
-	const colorIconButton = createTheme({
-		palette: {
-			primary: {
-				main: "#9CA3AF",
-			},
-		},
-	});
+  const colorIconButton = createTheme({
+    palette: {
+      primary: {
+        main: "#9CA3AF",
+      },
+    },
+  });
 
-	const profileIcon =
-		"https://minimal-kit-react.vercel.app/static/mock-images/avatars/avatar_default.jpg";
+  const profileIcon =
+    "https://minimal-kit-react.vercel.app/static/mock-images/avatars/avatar_default.jpg";
 
-	return (
-		<header className="container">
-			<h1>DashFront</h1>
+  return (
+    <header className="container">
+      <h1>DashFront</h1>
 
-			<div className="icons">
-				<ThemeProvider theme={colorIconButton}>
-					<IconButton>
-						<SearchIcon color="primary" />
-					</IconButton>
+      <div className="icons">
+        <ThemeProvider theme={colorIconButton}>
+          <IconButton>
+            <SearchIcon color="primary" />
+          </IconButton>
 
-					<IconButton>
-						<NotificationsIcon color="primary" />
-					</IconButton>
+          <IconButton>
+            <NotificationsIcon color="primary" />
+          </IconButton>
 
-					<Button aria-describedby={id} onClick={handleClick}>
-						<img src={profileIcon} alt="Imagem user" />
-					</Button>
-					<Popper id={id} open={open} anchorEl={anchorEl}>
-						<div className="propper-profile">
-							<div className="profile">
-								<Avatar
-									alt="Profile image"
-									src={profileIcon}
-									sx={{ width: 56, height: 56 }}
-								/>
+          <Button aria-describedby={id} onClick={handleClick}>
+            <img src={profileIcon} alt="Imagem user" />
+          </Button>
+          <Popper id={id} open={open} anchorEl={anchorEl}>
+            <div className="propper-profile">
+              <div className="profile">
+                <Avatar
+                  alt="Profile image"
+                  src={profileIcon}
+                  sx={{ width: 56, height: 56 }}
+                />
 
-								<div className="info-account">
-									<p>{username}</p>
-									<p className="smaller">Acme Inc.</p>
-								</div>
-							</div>
+                <div className="info-account">
+                  <p>{username}</p>
+                  <p className="smaller">Acme Inc.</p>
+                </div>
+              </div>
 
-							<div className="options">
-								<div>
-									<Button>
-										<AccountCircleIcon color="primary" />
-										<p>Perfil</p>
-									</Button>
-								</div>
+              <div className="options">
+                <div>
+                  <Button>
+                    <AccountCircleIcon color="primary" />
+                    <p>Perfil</p>
+                  </Button>
+                </div>
 
-								<div>
-									<Button>
-										<SettingsIcon color="primary" />
-										<p>Configurações</p>
-									</Button>
-								</div>
-							</div>
+                <div>
+                  <Button>
+                    <SettingsIcon color="primary" />
+                    <p>Configurações</p>
+                  </Button>
+                </div>
 
-							<div className="logout">
-								<div>
-									<Button>
-										<LogoutIcon />
+                <div>
+                  <Link to={`/admin/${username}`}>
+                    <Button>
+                      <HomeIcon color="primary" />
+                      <p>Overview</p>
+                    </Button>
+                  </Link>
+                </div>
+              </div>
 
-										<p>Sair</p>
-									</Button>
-								</div>
-							</div>
-						</div>
-					</Popper>
-				</ThemeProvider>
-			</div>
-		</header>
-	);
+              <div className="logout">
+                <div>
+                  <Link to="/">
+                    <Button>
+                      <LogoutIcon />
+
+                      <p className="text-logout">Sair</p>
+                    </Button>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </Popper>
+        </ThemeProvider>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
